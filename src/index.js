6834d64c76eeafef9c2e2119262b41b2c2ee1350
marc berger <marc.berger75020@gmail.com>
2019-04-19 19:41:16 +0200

    adjust the height to 100%, adjust the space-between of panel-container

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
-const element = <App />;
-ReactDOM.render(element, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
